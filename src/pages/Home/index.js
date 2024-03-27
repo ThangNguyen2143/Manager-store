bc27e641e5332c6f70b4ccab4475227134429ecb
NguyenNLong <163871066+NguyenNLong@users.noreply.github.com>
2024-03-27 11:36:14 +0700

    Header-Footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '~/components/Header';
 import Footer from '~/components/Footer';
+import Body from '~/components/Body';
 export default class Home extends Component {
   render() {
     return (
       <div>
         <Header></Header>
-        <h1>Body ở đây</h1>
-        <h1>Body ở đây</h1>
-        <h1>Body ở đây</h1>
-        <h1>Body ở đây</h1>
-        <h1>Body ở đây</h1>
-        <h1>Body ở đây</h1>
+        <Body></Body>
         <Footer></Footer>
       </div>
     );
